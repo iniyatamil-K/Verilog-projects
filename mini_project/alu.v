eb55a49f0a278c794294a6af55038ebf1bf2ab4b
Iniyatamil <iniyatamilk2024@gmail.com>
2026-03-23 19:24:19 +0530

    Update alu.v

--- mini_project/alu.v
+++ mini_project/alu.v
@@ -1,1 +1,16 @@
+module alu (
+    input [3:0] A, B,
+    input [1:0] Sel,
+    output reg [3:0] Y
+);
 
+always @(*) begin
+    case (Sel)
+        2'b00: Y = A + B;
+        2'b01: Y = A - B;
+        2'b10: Y = A & B;
+        2'b11: Y = A | B;
+    endcase
+end
+
+endmodule
