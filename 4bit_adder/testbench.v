602777d46422303ac3a6de4719d2364b29afdad0
iniyatamil-K <iniyatamilk2024@gmail.com>
2026-03-23 15:25:20 +0530

    Create testbench.v

---
+++ 4bit_adder/testbench.v
@@ -0,0 +1,22 @@
+module tb_adder;
+reg [3:0] A, B;
+reg Cin;
+wire [3:0] Sum;
+wire Cout;
+
+adder_4bit uut (
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .Sum(Sum),
+    .Cout(Cout)
+);
+
+initial begin
+    A = 4'b0000; B = 4'b0000; Cin = 0;
+    #10 A = 4'b0011; B = 4'b0101;
+    #10 A = 4'b1111; B = 4'b0001;
+    #10 $finish;
+end
+
+endmodule
