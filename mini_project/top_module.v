aa14413546df0ea0a753c3ff48d234423a1b33f6
Iniyatamil <iniyatamilk2024@gmail.com>
2026-03-23 19:24:59 +0530

    Update top_module.v

--- mini_project/top_module.v
+++ mini_project/top_module.v
@@ -1,1 +1,22 @@
+module top_module (
+    input clk,
+    input reset,
+    input [3:0] A, B,
+    output [3:0] Y,
+    output [1:0] Sel
+);
 
+fsm_control fsm (
+    .clk(clk),
+    .reset(reset),
+    .Sel(Sel)
+);
+
+alu alu_unit (
+    .A(A),
+    .B(B),
+    .Sel(Sel),
+    .Y(Y)
+);
+
+endmodule
