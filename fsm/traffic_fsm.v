37b4e3609e6bedd3e237d2d9bf029205706423ed
Iniyatamil <iniyatamilk2024@gmail.com>
2026-03-23 18:52:03 +0530

    Update traffic_fsm.v

--- fsm/traffic_fsm.v
+++ fsm/traffic_fsm.v
@@ -3,13 +3,14 @@ module traffic_fsm (
     input reset,
     output reg [1:0] state
 );
+
 parameter S0 = 2'b00; // Green
 parameter S1 = 2'b01; // Yellow
 parameter S2 = 2'b10; // Red
 
 always @(posedge clk or posedge reset) begin
     if (reset)
-        state <= S0;
+        state <= S0;   // Proper reset
     else begin
         case (state)
             S0: state <= S1;
