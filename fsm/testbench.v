010ebc0fb04c07cbc839049bfa23b96e14985295
Iniyatamil <iniyatamilk2024@gmail.com>
2026-03-23 18:53:51 +0530

    Update testbench.v

--- fsm/testbench.v
+++ fsm/testbench.v
@@ -10,7 +10,9 @@ traffic_fsm uut (
     .state(state)
 );
 
+// Clock generation (10 time units period)
 always #5 clk = ~clk;
+
 initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, tb_fsm);
@@ -18,7 +20,8 @@ initial begin
     clk = 0;
     reset = 1;
 
-    #10 reset = 0;
+    // Keep reset active for enough time
+    #12 reset = 0;
 
     #100 $finish;
 end
