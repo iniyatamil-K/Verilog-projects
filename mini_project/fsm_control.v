fca6c8fb18abfc88f96236a87bc002495ae300a7
Iniyatamil <iniyatamilk2024@gmail.com>
2026-03-23 19:23:33 +0530

    Update fsm_control.v

--- mini_project/fsm_control.v
+++ mini_project/fsm_control.v
@@ -1,1 +1,14 @@
+module fsm_control (
+    input clk,
+    input reset,
+    output reg [1:0] Sel
+);
 
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        Sel <= 2'b00;
+    else
+        Sel <= Sel + 1; // cycles 00 → 01 → 10 → 11
+end
+
+endmodule
